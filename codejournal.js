f644852efd9b4e6b20ad867aecec52e51b72f0f9
Katelyn Swain <k8lynswain@gmail.com>
2022-02-24 12:42:46 -0700

    added films directory and new image

---
+++ codejournal.js
@@ -0,0 +1,86 @@
+// variables = containers that store values
+
+var name; // = declared, but not initialized variable and is in the golbal scope (BAD)
+
+let foo; // = declared variable that can be changed
+
+const bar = "Bar"; // = declared variable that can't be changed- short for "constant"
+
+// "=" is he assigment operator, read it as "is assigned the value of..."
+
+const ANSWER = 42;
+
+// Strings = collection of characters
+
+let string1 = "Hello World"; 
+
+string1 = "Hello Utah";  // let allows me to change the value of the variable
+
+let string3 = new String("Hellow New World!");
+
+// Numbers
+
+let myNum = 2384975964;
+
+let floatNum = 75.25;
+
+"1" == 1; // because of type coertion and loose equality checking, this is true
+
+"1" === 1; // false, because this is strict equality checking
+
+
+// Boolian
+
+let myBool = false;
+
+// check into truthy and falsy values
+
+// Array
+
+let myArray = []; // this is an empty array
+
+// 0 indexed:   0     1      2       3       4
+let myArray2 = [42, "Bob", myBool, ANSWER, true];
+
+let lastItem = myArray2[myArray2,length-1]
+
+// Objects
+
+let minObject = {}; // this is the most minimal javascript object you can have
+
+let myCar = { // js objects are comprised of key value pairs
+    make: 'Jeep',
+    model: 'Cherokee',
+    year: '1998',
+    vin: '234904352349',
+}
+
+let newVar = myCar.make //used dot notation (.) to acces an objects property value
+
+myCar.numDoors = 6; // can also use dot notation to add a property to an object
+
+const anotherObject = {
+    wordz: ['foo', 'bar', 'baz'],
+    car: {
+        make: 'McLaren',
+        model: '720S',
+    },
+    awesomeness: true
+}
+
+// Functions
+
+function myFunction() {
+    return "My greeting to you from this very fine, simple function!"
+}
+
+function sumTwoThings(thing1, thing2) {
+    return thing1 + thing2;
+}
+
+
+
+
+
+
+
