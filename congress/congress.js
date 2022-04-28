44a189e1141f469077fb2e973a1314ad2fb7a461
Katelyn Swain <k8lynswain@gmail.com>
2022-04-28 14:07:51 -0600

    added state to senators and started party filter

--- congress/congress.js
+++ congress/congress.js
@@ -1,68 +1,72 @@
-import { senators } from "../data/senators.js";
-import { representatives } from "../data/representatives.js";
+import { senators } from '../data/senators.js'
+import { representatives } from '../data/representatives.js'
 
-const allCongressMembers = [...senators, ...representatives];
+const allMembersOfCongress = [...senators, ...representatives] // modern combining of array data... like a genius!
 
-const senatorDiv = document.querySelector(".senatorsDiv");
-const seniorityHeading = document.querySelector(".seniorityHeader");
-const loyaltyList = document.querySelector(".loyaltyList");
+const senatorsDiv = document.querySelector('.senatorsDiv')
+const seniorityHeader = document.querySelector('.seniority')
+const loyaltyList = document.querySelector('.loyaltyList')
 
 function simplifiedSenators() {
-  return senators.map((senator) => {
-    const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `;
+  return senators.map(senator => {
+    const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
     return {
       id: senator.id,
       name: `${senator.first_name}${middleName}${senator.last_name}`,
-      party: senator.party,
       gender: senator.gender,
+      party: senator.party,
       imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
       seniority: +senator.seniority,
+      state: senator.state,
       missedVotesPct: senator.missed_votes_pct,
-      loyaltyPct: senator.votes_with_party_pct,
-    };
-  });
+      loyaltyPct: senator.votes_with_party_pct
+    }
+  })
 }
 
-const simpleSenators = simplifiedSenators();
+function populateSenatorDiv(senatorsArray) {
+  senatorsArray.forEach(senator => {
+    const senFigure = document.createElement('figure')
+    const figImg = document.createElement('img')
+    const figCaption = document.createElement('figcaption')
 
-function populateSenatorDiv(senatorArray) {
-  senatorArray.forEach((senator) => {
-    const senFigure = document.createElement("figure");
-    const figImg = document.createElement("img");
-    const figCaption = document.createElement("figcaption");
+    figImg.src = senator.imgURL
+    figCaption.textContent = senator.name + '- ' + senator.state
 
-    figImg.src = senator.imgURL;
-    figCaption.textContent = senator.name;
-
-    senFigure.appendChild(figImg);
-    senFigure.appendChild(figCaption);
-    senatorDiv.appendChild(senFigure);
-  });
+    senFigure.appendChild(figImg)
+    senFigure.appendChild(figCaption)
+    senatorsDiv.appendChild(senFigure)
+  })
 }
 
-populateSenatorDiv(simpleSenators);
+populateSenatorDiv(simplifiedSenators())
 
-const mostSeniorMember = simplifiedSenators().reduce((acc, senator) => {
-  return acc.seniority > senator.seniority ? acc : senator;
-});
+const mostSeniorMember = simplifiedSenators().reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
 
-const biggestMissedVotesPct = simplifiedSenators().reduce((acc, senator) =>
-  acc.missedVotesPct > senator.missedVotesPct ? acc : senator
-);
+const biggestMissedVotesPct = simplifiedSenators().reduce((acc, senator) => acc.missedVotesPct > senator.missedVotesPct ? acc : senator)
 
-const biggestVacationerList = simplifiedSenators()
-  .filter(
-    (senator) => senator.missedVotesPct === biggestMissedVotesPct.missedVotesPct
-  )
-  .map((senator) => senator.name)
-  .join(" and ");
+const biggestVactionerList = simplifiedSenators().filter(senator => senator.missedVotesPct === biggestMissedVotesPct.missedVotesPct).map(senator => senator.name).join(' and ')
 
-seniorityHeading.textContent = `The most senior member of the senate is ${mostSeniorMember.name} and the biggest vacationers are ${biggestVacationerList}`;
+seniorityHeader.textContent = `The most senior Senator is ${mostSeniorMember.name} and the biggest fans of vacations are ${biggestVactionerList}.`
 
-simplifiedSenators().forEach((senator) => {
-  if (senator.loyaltyPct === 100) {
-    let listItem = document.createElement("li");
-    listItem.textContent = senator.name;
-    loyaltyList.appendChild(listItem);
+simplifiedSenators().forEach(senator => {
+  if(senator.loyaltyPct === 100) {
+    let listItem = document.createElement('li')
+    listItem.textContent = senator.name
+    loyaltyList.appendChild(listItem)
   }
-});
+}
+
+)
+
+/*function filterSenatorByParty(party) {
+  return senatorsArray.filter((senator) => senator.party === party)
+}
+
+const partySelect = document.querySelector('.partySelect')
+partySelect.addEventListener('change', (event) => {
+  const usersTypeChoice = event.target.value.toLowerCase()
+  const senatorByParty = filterSenatorByParty(usersTypeChoice)
+  removeChildren(pokeGrid)
+  senatorByParty.forEach((partysentator) => populatePokeCard(party))
+})*/
