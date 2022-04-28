d5f54f921afe2a10f328b7b3a1e3e3754c07f764
Katelyn Swain <k8lynswain@gmail.com>
2022-04-27 21:41:06 -0600

    fixed pokemon whoot!

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -247,7 +247,7 @@ typeSelect.addEventListener('change', (event) => {
 })
 
 function calculateHP() {
-  const mostHP = loadedPokemon.reduce((acc, pokemon) => acc.hp > pokemon.hp ? acc : pokemon, {})
+  const mostHP = loadedPokemon.reduce((acc, pokemon) => acc.hp > pokemon.hp ? acc : pokemon, {})}
 
   const messageArea = document.querySelector('.messageArea')
   messageArea.textContent = `${mostHP.name[0].toUpperCase()}${mostHP.name.substring(1)} has the most HP at ${mostHP.hp}`
